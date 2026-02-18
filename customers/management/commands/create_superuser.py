10f449cebf7d57e465be3eca78ce5721c1a74399
costleyosaro <costleyosaro4@gmail.com>
2026-02-18 11:18:19 +0100

    Fix superuser with business_name field

--- customers/management/commands/create_superuser.py
+++ customers/management/commands/create_superuser.py
@@ -3,21 +3,35 @@ from django.contrib.auth import get_user_model
 
 
 class Command(BaseCommand):
-    help = 'Create default superuser'
+    help = 'Create default superuser and reset login locks'
 
     def handle(self, *args, **options):
+        # Reset axes lockouts
+        try:
+            from axes.models import AccessAttempt, AccessLog, AccessFailureLog
+            AccessAttempt.objects.all().delete()
+            self.stdout.write(self.style.SUCCESS('Login locks cleared!'))
+        except Exception as e:
+            self.stdout.write(self.style.WARNING(f'Could not clear locks: {e}'))
+        
+        # Create superuser
         User = get_user_model()
         
-        business_name = 'costley'
-        email = 'costleyosaro4@gmail.com'
-        password = 'Osayi@12!'
+        business_name = 'ChiamoAdmin'
+        email = 'admin@chiamoorder.com'
+        password = 'ChiamoAdmin2025!'
         
-        if not User.objects.filter(business_name=business_name).exists():
-            User.objects.create_superuser(
+        if User.objects.filter(business_name=business_name).exists():
+            # Update existing user password
+            user = User.objects.get(business_name=business_name)
+            user.set_password(password)
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'Password reset for "{business_name}"!'))
+        else:
+            # Create new superuser
+            user = User.objects.create_superuser(
                 business_name=business_name,
                 email=email,
                 password=password
             )
             self.stdout.write(self.style.SUCCESS(f'Superuser "{business_name}" created!'))
-        else:
-            self.stdout.write(self.style.WARNING(f'Superuser "{business_name}" already exists.'))
