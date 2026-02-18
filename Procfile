19bf452eaa8438e6753bdf0a000a24d63f220f71
costleyosaro <costleyosaro4@gmail.com>
2026-02-18 10:50:03 +0100

    Add auto superuser creation

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py collectstatic --noinput && python manage.py migrate && gunicorn chiamo_project.wsgi --log-file -
+web: python manage.py migrate && python manage.py create_superuser && python manage.py collectstatic --noinput && gunicorn chiamo_project.wsgi --log-file -
---
+++ customers/management/commands/create_superuser.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = 'Create default superuser'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        
+        business_name = 'costley'
+        email = 'costleyosaro4@gmail.com'
+        password = 'Osayi@12!'
+        
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                business_name=business_name,
+                email=email,
+                password=password
+            )
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{business_name}" created!'))
+        else:
+            self.stdout.write(self.style.WARNING(f'Superuser "{business_name}" already exists.'))
