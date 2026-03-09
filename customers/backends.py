938fc36fc78f402a005b74117c87a9c1256ae8aa
costleyosaro <costleyosaro4@gmail.com>
2026-03-09 15:15:37 +0100

    fixing the settings and backend.py file

---
+++ customers/backends.py
@@ -0,0 +1,27 @@
+﻿from django.contrib.auth.backends import ModelBackend
+from .models import User
+
+class BusinessNameBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, business_name=None, **kwargs):
+        identifier = username or business_name
+        if not identifier or not password:
+            return None
+        try:
+            user = User.objects.get(business_name=identifier)
+        except User.DoesNotExist:
+            try:
+                user = User.objects.get(business_name__iexact=identifier)
+            except User.DoesNotExist:
+                try:
+                    user = User.objects.get(email__iexact=identifier)
+                except User.DoesNotExist:
+                    return None
+        if user and user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
